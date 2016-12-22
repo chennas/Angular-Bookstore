34cd87345d11a63e5b3b963f80a58e05e7311cc1
SivaKumar <SivaKumar>
2016-12-22 22:00:24 +0530

    Initial commit for BookStore Application

---
+++ Bookart/Scripts/app.js
@@ -0,0 +1,10 @@
+﻿/// <reference path="angular.min.js" />
+
+var myApp = angular.module("myApp", []);
+
+myApp.controller("HeaderController", function ($scope) {
+    $scope.appDetails = {
+        title: "Bookart",
+        tagline: "We have 1 million books for you"
+    };
+});
